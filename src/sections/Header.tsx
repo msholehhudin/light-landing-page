29fd70005cdc29d50026b9e184c090b48f2d1dce
Muhammad  Sholehhudin <msholehhudin.ms@gmail.com>
2024-09-24 15:05:47 +0700

    Navbar is Done

---
+++ src/sections/Header.tsx
@@ -0,0 +1,36 @@
+import ArrowRight from "@/assets/arrow-right.svg";
+import Image from "next/image";
+import Logo from "@/assets/logosaas.png";
+import MenuIcon from "@/assets/menu.svg";
+
+export const Header = () => {
+  return (
+    <header className="sticky top-0">
+      <div className=" bg-black flex justify-center items-center py-3 text-white text-sm">
+        <div className="inline-flex items-center  gap-4">
+          <p>Dapatkan Informasi Selengkapnya</p>
+          <ArrowRight className="h-4 2-4 hover:translate-x-2 hover:cursor-pointer justify-center items-center" />
+        </div>
+      </div>
+
+      <div className="py-5 border border-red-500 ">
+        <div className="container">
+          <div className="flex justify-between items-center">
+            <Image src={Logo} alt="Logo" height={40} width={40} />
+            <MenuIcon className="h-5 w-5 md:hidden" />
+            <nav className="text-black/60 gap-6 md:flex hidden items-center">
+              <a href="#">About</a>
+              <a href="#">Feature</a>
+              <a href="#">Customers</a>
+              <a href="#">Updates</a>
+              <a href="#">Help</a>
+              <button className="bg-black text-white px-4 py-2 rounded-lg inline-flex items-center">
+                Contact Us
+              </button>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
